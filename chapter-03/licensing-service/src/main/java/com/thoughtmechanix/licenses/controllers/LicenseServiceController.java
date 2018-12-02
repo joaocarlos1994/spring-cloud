947ef7545af94fc141d601c1f313520c02f6560e
João Carlos Batista <joao_nad@hotmail.com>
2018-12-02 14:31:21 -0200

    It was add licensing service server and some depedency in pom.xml

---
+++ chapter-03/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -0,0 +1,28 @@
+package com.thoughtmechanix.licenses.controllers;
+
+import com.thoughtmechanix.licenses.model.License;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/***
+ *A {@code {@link RestController}} tells Spring Boot this is a REST-based
+ * services and will automatically serialize/deserialize service request/response to JSON.
+ *
+ */
+@RestController
+@RequestMapping(value="/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+    @GetMapping(value="/{licenseId}")
+    public License getLicenses(@PathVariable("organizationId") final String organizationId,
+                               @PathVariable("licenseId") final String licenseId) {
+        return new License()
+                .withId(licenseId)
+                .withOrganizationId(organizationId)
+                .withProductName("Teleco")
+                .withLicenseType("Seat");
+    }
+}
