09dc03b5fc5cf96c8104270bb34faac0f5a7e157
João Carlos Batista <joao_nad@hotmail.com>
2019-01-08 17:13:10 -0200

    Finalizando item 3.3

--- chapter-03/confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
+++ chapter-03/confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -3,17 +3,17 @@ package com.thoughtmechanix.confsvr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * A {@code {@link EnableConfigServer}} enables the service as a Spring Cloud Config service.
  * */
+@RefreshScope
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
-    //I stoppped 3.2.2
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
-
 }
