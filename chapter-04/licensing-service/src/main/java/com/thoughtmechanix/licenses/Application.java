8d1bf3e3a6e534cf78a12a28a2aa915ac71c343a
João Carlos Batista <joao_nad@hotmail.com>
2019-01-09 17:46:45 -0200

    It was created another project EurekaServer and Organization Server

--- chapter-04/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ chapter-04/licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -2,12 +2,13 @@ package com.thoughtmechanix.licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * This {@code {@link SpringBootApplication}} tells the Spring Boot framework
  * that this is the bootstrap class for the project
  * */
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
     
--- chapter-04/organization-service/src/main/docker/run.sh
+++ chapter-04/organization-service/src/main/docker/run.sh
@@ -1,8 +1,4 @@
-
 #!/bin/sh
-getPort() {
-    echo $1 | cut -d : -f 3 | xargs basename
-}
 
 echo "********************************************************"
 echo "Waiting for the eureka server to start on port $EUREKASERVER_PORT"
@@ -23,11 +19,11 @@ echo "********************************************************"
 while ! `nc -z configserver $CONFIGSERVER_PORT`; do sleep 3; done
 echo "*******  Configuration Server has started"
 
-
 echo "********************************************************"
-echo "Starting License Server with Configuration Service via Eureka :  $EUREKASERVER_URI" ON PORT: $SERVER_PORT;
+echo "Starting Organization Service  "
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom -Dserver.port=$SERVER_PORT   \
      -Deureka.client.serviceUrl.defaultZone=$EUREKASERVER_URI             \
      -Dspring.cloud.config.uri=$CONFIGSERVER_URI                          \
-     -Dspring.profiles.active=$PROFILE -jar /usr/local/licensingservice/@project.build.finalName@.jar
+     -Dspring.profiles.active=$PROFILE                                   \
+     -jar /usr/local/organizationservice/@project.build.finalName@.jar
