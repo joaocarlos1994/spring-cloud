3a3af2361fe88eab9186910de3c1efbd5e8f087a
joaocarlos1994 <joao_nad@hotmail.com>
2019-11-15 19:43:00 -0300

    Chapter 9 initial commit

--- chapter-08/specialroutes-service/src/main/java/com/thoughtmechanix/specialroutes/repository/AbTestingRouteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thoughtmechanix.specialroutes.repository;
-
-import com.thoughtmechanix.specialroutes.model.AbTestingRoute;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface AbTestingRouteRepository extends CrudRepository<AbTestingRoute,String>  {
-    public AbTestingRoute findByServiceName(String serviceName);
-}
